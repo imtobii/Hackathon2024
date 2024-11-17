432e93c942e8fec9b8c41923c9e85998580933e2
MUNDANE-MONDAYS <jiyoopark05@gmail.com>
2024-11-16 23:18:17 -0600

    Delete data_page.py

--- data_page.py
+++
@@ -1,5 +0,0 @@
-import streamlit as st
-import pandas as pd
-from io import StringIO
-
-st.markdown("<h1 style='text-align: center; color: Black; margin-top: 100px;'>EOG HYDRATE ALERT</h1>", unsafe_allow_html=True)
