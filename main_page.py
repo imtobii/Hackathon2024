c8d09bcc4f96267bf8ccdb7840b04880f4785fac
MUNDANE-MONDAYS <jiyoopark05@gmail.com>
2024-11-17 02:09:40 -0600

    Update main_page.py

--- main_page.py
+++ main_page.py
@@ -2,33 +2,29 @@ import streamlit as st
 import pandas as pd
 from io import StringIO
 
-st.markdown("<h1 style='text-align: center; color: Black; margin-top: 100px;'>EOG HYDRATE ALERT</h1>", unsafe_allow_html=True)
+
+st.markdown("<h1 style='text-align: center; color: Black; margin-top: 100px; font-family:Arial, Times, serif;'>EOG HYDRATE ALERT</h1>", unsafe_allow_html=True)
 
 
 
 uploaded_file = st.file_uploader("Choose a file")
 if uploaded_file is not None:
-    # To read file as bytes:
-    bytes_data = uploaded_file.getvalue()
-    st.write(bytes_data)
-
-    # To convert to a string based IO:
-    stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
-    st.write(stringio)
-
-    # To read file as string:
-    string_data = stringio.read()
-    st.write(string_data)
-
-    # Can be used wherever a "file-like" object is accepted:
-    dataframe = pd.read_csv(uploaded_file)
-    st.write(dataframe)
+    # Save the uploaded file in session state
+    st.session_state["uploaded_file"] = uploaded_file
+    st.success("File uploaded successfully! Navigate to the Data Analysis Page to visualize the data.")
+else:
+    st.warning("Please upload a file to proceed.")
+    
+with st.container():
+    st.image("background.jpg")
 
 col1, col2, col3, col4,col5,col6 = st.columns(6)
 
 
+   
 
 with col6:
     
 
     st.page_link("pages\data_page.py", label="Analyze")
+
