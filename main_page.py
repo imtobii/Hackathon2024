d936ddb8fcdbb251f1abee3874b635275cf52d1f
MUNDANE-MONDAYS <jiyoopark05@gmail.com>
2024-11-17 06:48:07 -0600

    Update main_page.py

--- main_page.py
+++ main_page.py
@@ -2,10 +2,23 @@ import streamlit as st
 import pandas as pd
 from io import StringIO
 
+def custom_warning(message):
+    st.markdown(f'''
+        <p style="
+            background-color:#ffffff;
+            color:#000000;
+            font-size:18px;
+            border-radius:5px;
+            padding:10px;">
+            {message}
+        </p>
+    ''', unsafe_allow_html=True)
+
+
 background_image = """
 <style>
 [data-testid="stAppViewContainer"] {
-    background-image: url("https://cdn.discordapp.com/attachments/1307572297506361364/1307654666158477452/d80ab04d9fba5fc9d212276f112e04938c39444d7aab11468daab127.png?ex=673b17cf&is=6739c64f&hm=0b9024ff5a9c0a9e5791cef02c0f8278cfc7a1dfde938e0b9f4bebccf6c90e46&");
+    background-image: url("https://media.istockphoto.com/id/1181555776/vector/pipe-background_2.jpg?s=612x612&w=0&k=20&c=4sBhQ2AaE2gbXgE7C76GyV5MiGAr_tn88gigezSZds4=");
     background-size: 100vw 120vh;  
     background-position: center;  
     background-repeat: no-repeat;
@@ -24,7 +37,7 @@ if uploaded_file is not None:
     st.session_state["uploaded_file"] = uploaded_file
     st.success("File uploaded successfully! Navigate to the Data Analysis Page to visualize the data.")
 else:
-    st.warning("Please upload a file to proceed.")
+    custom_warning("Please upload a file to proceed.")
     
 # with st.container():
 #     st.image("background.jpg")
@@ -32,4 +45,4 @@ else:
 col1, col2, col3, col4,col5,col6 = st.columns(6)
 
 with col6:
-    st.page_link("pages\data_page.py", label="Analyze")
+    st.page_link("pages\data_page.py", label=":grey-background[Analyze]")
